1aeeb639e9667ae5dbceaeb4194db87e684b087c
xingzhe <xingzhe290@foxmail.com>
2020-09-12 17:39:06 +0800

    完成会话管理

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require('express')
-const router = require('./router')
-const bodyParser = require('body-parser')
+const express = require("express");
+const session = require("express-session")
+const router = require("./router");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+/**
+ * 配置解析表单请求体
+ */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/**
+ * 配置使用 session
+ */
+app.use(
+	session({
+        secret: "keyboard cat",
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 
 // 把路由应用到 app 中
-app.use(router)
+app.use(router);
 
 // 统一处理 500 错误
 app.use((err, req, res, next) => {
-    res.status(500).json({
-        error: err.message
-    })
-})
+	res.status(500).json({
+		error: err.message,
+	});
+});
 
 app.listen(3000, () => {
-    console.log('App is running at port 3000')
-    console.log('Please visit http://127.0.0.1:3000')
-})
+	console.log("App is running at port 3000");
+	console.log("Please visit http://127.0.0.1:3000");
+});
--- controllers/session.js
+++ controllers/session.js
@@ -1,11 +1,63 @@
+const md5 = require('blueimp-md5')
+const session = require('express-session')
+const db = require('../models/db')
+
+/**
+ * 获取会话状态
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 exports.get = (req, res, next) => {
-
+    const {user} = req.session
+    if (!user) {
+        res.status(401).json({
+            error: 'Unauthorized'
+        })
+    }
+    res.status(200).json(user)
 }
 
-exports.create = (req, res, next) => {
+/**
+ * 创建会话：用户登录
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+exports.create = async (req, res, next) => {
+    // 接收表单请求
+    // 操作数据库处理登录请求
+    // 发送响应
+    try {
+        const body = req.body
+        body.password = md5(md5(body.password))
 
+        const sqlStr = `select * from users where email = '${body.email}' and password = '${body.password}'`
+        const [user] = await db.query(sqlStr)
+
+        if (!user) {
+            return res.status(404).json({
+                error: 'Invalid email or password!'
+            })
+        }
+
+        // 登录成功，记录 Session
+        req.session.user = user
+        
+        // 发送响应
+        res.status(201).json(user)
+    } catch (err) {
+        next(err)
+    }
 }
 
+/**
+ * 注销登录
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
 exports.destroy = (req, res, next) => {
-
+    delete req.session.user
+    res.status(201).json({})
 }
